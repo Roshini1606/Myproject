bd5f985653d09b0ba096487839948c429f7e82d9
Roshini N <roshininpoojary@gmail.com>
2024-09-04 15:25:37 +0530

    function2 added

--- main.java
+++ main.java
@@ -2,4 +2,12 @@ class Demo{
 public static void main(String args[]){
 System.out.println("hello");
 }
+public void f1(){
+    for(int i=0;i<=10;i++){
+        System.out.println(i);
+    }
+}
+public void f2(){
+    System.out.println("hello");
+}
 }
